36c63fb295e64fb86a52082a11f9671bb4ae51f8
Nuthanmani <88588757+Nuthanmani@users.noreply.github.com>
2021-10-27 22:44:19 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+long long compute_hash(char *s) 
+{
+    const int p = 31;
+    const int m = 1e9 + 9;
+    long long hashvalue = 0;
+    long long pow = 1;
+    long long k=strlen(s);
+    for (int i=0;i<k;i++) 
+    {
+        hashvalue = (hashvalue + (s[i] - 'A' + 1) * pow) % m;
+        pow = (pow * p) % m;
+    }
+    return hashvalue;
+}
+int main()
+{
+    char s[1000];
+    printf("Enter the String (upper case)\n");
+    scanf("%[^\n]s",s);
+    printf("Hash value: %ld",compute_hash(s));
+}
